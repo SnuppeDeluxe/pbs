baa749e98c11aaf9c806b6639c5356ec90f1e6f0
Kay <kay@hp.de>
2022-06-24 16:01:01 +0200

    Express installiert und Basics eingerichtet.

---
+++ app.js
@@ -0,0 +1,29 @@
+//Imports von Node Modulen
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+//Imports von ausgelagerten Funktionen
+//const adminRoutes = require("./routes/admin.js");
+//const shopRoutes = require("./routes/shop.js");
+
+//Express starten
+const app = express();
+
+//Body-Parser für req.body (Funktioniert nicht wie gewünscht)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Lesezugriff auf das Filesystem
+app.use(express.static(path.join(__dirname, "public")));
+
+//Funktionen benutzen
+//app.use(adminRoutes.routes);
+//app.use(shopRoutes);
+
+//Page not found
+app.use("/", (req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+});
+
+//Port öffnen
+app.listen(3000); //Erstellt den Server und öffnet den Port
